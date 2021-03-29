16fdb81b91176070526068d78043594a84118515
juanalma <juanlah31@gmail.com>
2021-03-29 15:42:17 +0700

    ok

---
+++ plugins/js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
+
+const enter = document.getElementById("enter");
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+  typeSpeed: 50,
+  backSpeed: 50,
+  smartBackspace: true,
+  showCursor: true,
+  cursorChar: "|",
+  loop: true,
+});
+
+document.addEventListener("mousemove", parallax);
+function parallax(e) {
+  this.querySelectorAll(".layer").forEach((layer) => {
+    const speed = layer.getAttribute("data-speed");
+
+    const x = (window.innerWidth - e.pageX * speed) / 100;
+    const y = (window.innerHeight - e.pageY * speed) / 100;
+
+    layer.style.transform = `translateX(${x}px) translate(${y}px)`;
+  });
+}
+
+
